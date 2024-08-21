3c9004ba90c8210e0430690cc4469d662ee9d620
leozhou <492753662@qq.com>
2024-08-21 18:41:41 +0800

    feat: fork from nextui-ts-shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import NextCard from '@/components/card';
 import Counter from '@/components/counter';
 import GithubCorner from '@/components/github-corner';
 
 export default function Home() {
   return (
-    <main className='flex h-full w-full flex-col items-center justify-center'>
+    <main className='flex h-full w-full flex-col items-center justify-center gap-2'>
+      <NextCard />
       <GithubCorner
         title='Get started on GitHub'
         url='https://github.com/doinel1a/next-ts-shadcn-ui'
